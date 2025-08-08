d348eb30d03ca1c1029b00a2178d04a40545dd6c
Fazal <mailsfazal@gmail.com>
2025-08-08 21:03:04 +0530

    initial commit

---
+++ puzzle.py
@@ -0,0 +1,63 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    Or(AKnight, AKnave), 
+    And(AKnave, Not(AKnight))
+)
+
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    Or(AKnave, BKnave),
+    And(BKnight, AKnave)
+)
+
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    Or(And(AKnight, BKnight), And(AKnave, BKnight)),
+    Or(And(BKnight, AKnave), And(BKnave, AKnave))
+)
+
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    And(Or(AKnight, AKnave), And(AKnight, Not(AKnave))),
+    Or(And(AKnave, BKnight), And(BKnave, Not(AKnave))),
+    Or(And(BKnight, CKnave), And(BKnave, Not(CKnave))),
+    Or(And(CKnight, AKnight), And(CKnave, Not(AKnight)))                   
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
